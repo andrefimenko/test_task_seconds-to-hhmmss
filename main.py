3c86948489adb2cac701a14c6d4014ff5e1c67ae
andrefimenko <96167014+andrefimenko@users.noreply.github.com>
2023-02-01 16:50:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 a = 49873751
 
+
 def res(a):
+    
     s = a % 60
     if s < 10:
         s = f"0{s}"
+        
     m = a // 60 % 60
     if m < 10:
         m = f"0{m}"
+        
     h = a // 60 // 60
     if h < 10:
         h = f"0{h}"
+        
     return f"{h}:{m}:{s}"
+        
 
 print(res(a))
